0ec36477620273ef23f1719674eb750e4f2efbb0
Allle98 <47569637+Allle98@users.noreply.github.com>
2022-07-06 10:21:42 +0200

    Add files via upload

---
+++ dynamic_dags/generator.py
@@ -0,0 +1,14 @@
+from jinja2 import Environment, FileSystemLoader
+import yaml
+import os
+
+file_dir = os.path.dirname(os.path.abspath(__file__))
+env = Environment(loader=FileSystemLoader(file_dir))
+template = env.get_template('template_dags.jinja2')
+
+for filename in os.listdir(file_dir):
+    if filename.endswith(".yaml"):
+        with open(f"{file_dir}/{filename}", "r") as config_file:
+            config = yaml.safe_load(config_file)
+            with open(f"dags/dynamic_dags/get_price_{config['dag_id']}.py", "w") as f:
+                f.write(template.render(config))
